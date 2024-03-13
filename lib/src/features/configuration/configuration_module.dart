c61feaf13658d0beca0776eb4ef075851b2181b0
leonardomaia <leuow95@gmail.com>
2024-03-13 17:20:34 -0400

    feat: core of configuration page

--- lib/src/features/configuration/configuration_module.dart
+++ lib/src/features/configuration/configuration_module.dart
@@ -1,3 +1,9 @@
 import 'package:flutter_modular/flutter_modular.dart';
+import 'package:sciencedex_challenge/src/features/configuration/configutarion_page.dart';
 
-class ConfigurationModule extends Module {}
+class ConfigurationModule extends Module {
+  @override
+  List<ModularRoute> get routes => [
+        ChildRoute("/", child: (context, args) => const ConfigurationPage()),
+      ];
+}
