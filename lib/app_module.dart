f3ad3fb9c32f9837a1e23b044ac420c1253b4316
leonardomaia <leuow95@gmail.com>
2024-03-13 17:20:52 -0400

    feat: appmodule

--- lib/app_module.dart
+++ lib/app_module.dart
@@ -1,12 +1,13 @@
 import 'package:flutter_modular/flutter_modular.dart';
 import 'package:sciencedex_challenge/src/features/configuration/configuration_module.dart';
+import 'package:sciencedex_challenge/src/features/configuration/configutarion_page.dart';
 
 class AppModule extends Module {
   @override
+  List<Bind<Object>> get binds => [];
+  @override
   List<ModularRoute> get routes => [
-        ModuleRoute(
-          "/configuration",
-          module: ConfigurationModule(),
-        )
+        ChildRoute("/", child: ((context, args) => const ConfigurationPage())),
+        ModuleRoute("/configuration", module: ConfigurationModule()),
       ];
 }
--- lib/app_widget.dart
+++ lib/app_widget.dart
@@ -1,10 +1,18 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_modular/flutter_modular.dart';
 
 class AppWidget extends StatelessWidget {
   const AppWidget({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return const MaterialApp();
+    // Modular.setInitialRoute("/");
+
+    return MaterialApp.router(
+      routerDelegate: Modular.routerDelegate,
+      routeInformationParser: Modular.routeInformationParser,
+      debugShowCheckedModeBanner: false,
+      theme: ThemeData(useMaterial3: true),
+    );
   }
 }
