b1223e8864c873977b8e9a1bad51adb0792632b3
leonardomaia <leuow95@gmail.com>
2024-03-15 17:06:30 -0400

    feat: added cagtegory to add

--- lib/src/features/configuration/entities/period_entity.dart
+++ lib/src/features/configuration/entities/period_entity.dart
@@ -2,6 +2,7 @@ class PeriodEntity {
   final String name;
   final DateTime startDate;
   final DateTime endDate;
+  final String category;
   final int goal1;
   final int goal2;
 
@@ -9,17 +10,19 @@ class PeriodEntity {
     required this.name,
     required this.startDate,
     required this.endDate,
+    required this.category,
     required this.goal1,
     required this.goal2,
   });
 
   factory PeriodEntity.fromJson(Map<String, dynamic> json) {
     return PeriodEntity(
-      name: json["name"],
+      name: json["name"] ?? "",
       startDate: DateTime.parse(json["startDate"]),
       endDate: DateTime.parse(json["endDate"]),
-      goal1: json["goal1"],
-      goal2: json["goal2"],
+      category: json["category"] ?? "",
+      goal1: json["goal1"] ?? "",
+      goal2: json["goal2"] ?? "",
     );
   }
 
@@ -28,6 +31,7 @@ class PeriodEntity {
       "name": name,
       "startDate": startDate.toIso8601String(),
       "endDate": endDate.toIso8601String(),
+      "category": category,
       "goal1": goal1,
       "goal2": goal2,
     };
