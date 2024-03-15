78027e05d41202ad45e2c9945bd4c066d98fa622
leonardomaia <leuow95@gmail.com>
2024-03-14 22:26:57 -0400

    feat: configuration feature module

--- lib/src/features/configuration/configuration_module.dart
+++ lib/src/features/configuration/configuration_module.dart
@@ -1,7 +1,14 @@
 import 'package:flutter_modular/flutter_modular.dart';
+import 'package:sciencedex_challenge/src/features/configuration/service/configuration_service_impl.dart';
 import 'package:sciencedex_challenge/src/features/configuration/ui/configuration_page.dart';
+import 'package:sciencedex_challenge/src/features/configuration/ui/controller/configuration_controller.dart';
 
 class ConfigurationModule extends Module {
+  @override
+  List<Bind<Object>> get binds => [
+        Bind.lazySingleton((i) => ConfigurationServiceImpl(localAdapter: i())),
+        Bind.lazySingleton((i) => ConfigurationController(service: i())),
+      ];
   @override
   List<ModularRoute> get routes => [
         ChildRoute("/", child: (context, args) => const ConfigurationPage()),
