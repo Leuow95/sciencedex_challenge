920935eb2fc8362102e216b5fbc4fe8dc8d12b90
leonardomaia <leuow95@gmail.com>
2024-03-14 22:26:44 -0400

    feat: added binds to appmodule

--- lib/app_module.dart
+++ lib/app_module.dart
@@ -1,10 +1,14 @@
 import 'package:flutter_modular/flutter_modular.dart';
+import 'package:sciencedex_challenge/src/adapters/local_adapter.dart';
+import 'package:sciencedex_challenge/src/adapters/local_adapter_impl.dart';
 import 'package:sciencedex_challenge/src/features/configuration/configuration_module.dart';
 import 'package:sciencedex_challenge/src/features/configuration/ui/configuration_page.dart';
 
 class AppModule extends Module {
   @override
-  List<Bind<Object>> get binds => [];
+  List<Bind<Object>> get binds => [
+        Bind<LocalAdapter>((i) => LocalAdapterImpl()),
+      ];
   @override
   List<ModularRoute> get routes => [
         ChildRoute("/", child: ((context, args) => const ConfigurationPage())),
